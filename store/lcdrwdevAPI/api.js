c9db224f68a850dbd83c6f95bf70b70c3f6271ae
Crowdbotics <engineering@crowdbotics.com>
2023-08-25 07:36:38 +0000

    Update store.

--- store/lcdrwdevAPI/api.js
+++ store/lcdrwdevAPI/api.js
@@ -14,24 +14,6 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return lcdrwdevAPI.post(`/api/v1/signup/`, payload)
 }
-function modules_articles_article_list(payload) {
-  return lcdrwdevAPI.get(`/modules/articles/article/`)
-}
-function modules_articles_article_create(payload) {
-  return lcdrwdevAPI.post(`/modules/articles/article/`, payload)
-}
-function modules_articles_article_retrieve(payload) {
-  return lcdrwdevAPI.get(`/modules/articles/article/${payload.id}/`)
-}
-function modules_articles_article_update(payload) {
-  return lcdrwdevAPI.put(`/modules/articles/article/${payload.id}/`, payload)
-}
-function modules_articles_article_partial_update(payload) {
-  return lcdrwdevAPI.patch(`/modules/articles/article/${payload.id}/`, payload)
-}
-function modules_articles_article_destroy(payload) {
-  return lcdrwdevAPI.delete(`/modules/articles/article/${payload.id}/`)
-}
 function rest_auth_login_create(payload) {
   return lcdrwdevAPI.post(`/rest-auth/login/`, payload)
 }
@@ -69,12 +51,6 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
-  modules_articles_article_list,
-  modules_articles_article_create,
-  modules_articles_article_retrieve,
-  modules_articles_article_update,
-  modules_articles_article_partial_update,
-  modules_articles_article_destroy,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
